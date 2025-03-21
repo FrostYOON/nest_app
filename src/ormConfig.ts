3543014d8495562e8a650c58c1fd2a6d09187879
FrostYOON <yys1423@naver.com>
2025-03-21 21:17:29 +0900

    ormConfig.ts 파일 수정: 데이터베이스 연결 정보를 환경 변수로 변경하여 설정 유연성 향상.

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,23 +1,23 @@
 import { join } from 'path';
 import { DataSource } from 'typeorm';
 import 'dotenv/config';
-import { DbConfigService } from './config/db/config.service';
-import { ConfigService } from '@nestjs/config';
+// import { DbConfigService } from './config/db/config.service';
+// import { ConfigService } from '@nestjs/config';
 import { PostViewSubscriber } from './modules/posts/subscribers/post-view.subscriber';
 import { CommentSubscriber } from './modules/comments/subscribers/comment.subscriber';
 import { UserSubscriber } from './modules/users/subscribers/user.subscriber';
 
 const entities = [join(__dirname, '/**/*.entity{.ts,.js}')];
 const migrations = [join(__dirname, './migrations/**/*{.ts,.js}')];
-const dbConfigService = new DbConfigService(new ConfigService());
+// const dbConfigService = new DbConfigService(new ConfigService());
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: dbConfigService.dbHost,
-  port: dbConfigService.dbPort,
-  username: dbConfigService.dbUser,
-  password: dbConfigService.dbPassword,
-  database: dbConfigService.dbName,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities,
   migrations,
   synchronize: false,
